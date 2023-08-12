0c300538fa851579384b2e39b6e9c6b4ff47ecf5
Kirill Vinogradov <61331094+Kirill-Vine@users.noreply.github.com>
2023-08-12 13:11:22 -0400

    finished

--- war-challenge/ContentView.swift
+++ war-challenge/ContentView.swift
@@ -8,14 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Image("background").ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button {
+                    // generate random number from 2 and 14 and update cards
+                    let playerRandom = String(Int.random(in: 2...14))
+                    let cpuRandom = String(Int.random(in: 2...14))
+                    playerCard = "card" + playerRandom
+                    cpuCard = "card" + cpuRandom
+                    // update score
+                    if cpuRandom<playerRandom{
+                        playerScore+=1
+                    }else if cpuRandom>playerRandom{
+                        cpuScore+=1
+                    }
+                } label: {
+                    Image("dealbutton")
+                }
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(
+                                .system(size: 30))
+                            .foregroundColor(Color.white)
+                            .padding(.bottom,10)
+                        Text(String(playerScore))
+                            .font(
+                                .system(size: 30))
+                            .foregroundColor(Color.white)
+                            .padding(.bottom)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(
+                                .system(size: 30))
+                            .foregroundColor(Color.white)
+                            .padding(.bottom,10)
+                        Text(String(cpuScore))
+                            .font(
+                                .system(size: 30))
+                            .foregroundColor(Color.white)
+                            .padding(.bottom)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
